d4c90ddddedf29d98c554ed1d59a91ce06c4281a
Turtel216 <papadimitrios216@gmail.com>
2025-06-17 20:00:07 +0300

    Ad purescript to doom

--- doom/init.el
+++ doom/init.el
@@ -154,7 +154,7 @@
        ;;php               ; perl's insecure younger brother
        ;;plantuml          ; diagrams for confusing people more
        ;;graphviz          ; diagrams for confusing yourself even more
-       ;;purescript        ; javascript, but functional
+       purescript        ; javascript, but functional
        ;;python            ; beautiful is better than ugly
        ;;qt                ; the 'cutest' gui framework ever
        ;;racket            ; a DSL for DSLs
